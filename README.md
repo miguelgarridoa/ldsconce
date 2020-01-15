b34fe2cf5bfdb3fa691c474f52d3e0edb34f7ab6
miguelgarridoa <miguel.garrido@ulare.cl>
2020-01-15 13:04:30 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ldsconce
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ldsconce)
---
+++ src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HeaderComponent } from './components/header/header.component';
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule ],
+  declarations: [ AppComponent, HelloComponent, NavbarComponent, HeaderComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
